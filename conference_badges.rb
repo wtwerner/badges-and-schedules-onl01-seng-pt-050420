78ed3fcf29ddd3d409a4141ed993f766fc1aa5a5
Tommy Werner <wtwerner@gmail.com>
2020-05-10 16:25:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,7 @@ def badge_maker(name)
 end
 
 def badge_maker_creator(attendees)
-  badges=[]
   names.each do |name|
-    badges << "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
   end
-  return badges
 end
